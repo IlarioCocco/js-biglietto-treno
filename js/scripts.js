ee3f170e37c461faa8209ca7c43337cb3907710b
IlarioCOCCO <ilario.cocco@gmail.com>
2021-07-29 18:57:35 +0200

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,37 @@
 var numeroKm = prompt("quanti km vuoi percorrere ?")
 
 // 2.chiedere l'età del passeggero.//
-var età = prompt("quanti anni hai ?")
+var eta = prompt("quanti anni hai ?")
 
 // 3.chiedere il prezzo totale del viaggio.//
 var prezzoBase = 0.21 * numeroKm;
-alert(prezzoBase);
+alert("prezzo base senza sconto" + prezzoBase);
 
-// 4.sconto minorenni
+// 4.sconto minorenni//
 var scontoMinorenni = prezzoBase / 100 * 20;
-alert(scontoMinorenni);
+alert("sconto minorenni" + scontoMinorenni );
 
-// 4.sconto over 65
+// 4.sconto over 65//
 var scontoOver65 = prezzoBase / 100 * 40;
-alert(scontoOver65);
+alert("over 65 " + scontoOver65);
+
+// 5.prezzo prezzoMinorenni//
+var prezzoMinorenni = prezzoBase - scontoMinorenni;
+
+// 6.prezzoOver65//
+var prezzoOver65 = prezzoBase - scontoOver65;
 
 
-// var prezzoTotale;
-// if ( ) {
-// } else {
+if (eta < 18) {
+    alert("sconti minorenni dato " + prezzoMinorenni);
 
-// }
+} else if (eta > 65) {
+    alert("sconti over 65 dato " + prezzoOver65);
+
+} else {
+    alert("prezzo base " + prezzoBase);
+}
+
 
 
 
