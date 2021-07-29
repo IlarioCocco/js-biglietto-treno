42e31bdf7693e86bf731c4ba3b2ef9db471b60ac
IlarioCOCCO <ilario.cocco@gmail.com>
2021-07-29 15:13:59 +0200

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-alert ("ciao")
+// 1.chiedere all'utente il numero di chilometri che vuole percorrere.//
+var numerokm = prompt("quanti km vuoi percorrere ?")
+
+// 2.chiedere l'età del passeggero.//
+var età = prompt("quanti anni hai?")
+
+// 3.chiedere il prezzo totale del viaggio.//
+var prezzoBiglietto = prompt("prezzo totale biglietto")
+
+
+
+
+
+
+// il prezzo del biglietto è definito in base ai km(0.21 € al km)
+// va applicato uno sconto del 20 % per i minorenni
+// va applicato uno sconto del 40 % per gli over 65.
