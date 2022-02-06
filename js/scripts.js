b36340558e936d167d3ed4ba803573b7e4879581
IlarioCOCCO <ilario.cocco@gmail.com>
2022-02-06 11:41:18 +0100

    recap basic

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,52 @@
+// il prezzo del biglietto è definito in base ai km(0.21 € al km)//
+// va applicato uno sconto del 30 % per i minorenni
+// va applicato uno sconto del 50 % per gli over 65.
+
+
 // 1.chiedere all'utente il numero di chilometri che vuole percorrere.//
-var numeroKm = prompt("quanti km vuoi percorrere ?")
+var numeroKm = parseInt(prompt("quanti km vuoi percorrere ?"));
+console.log(typeof(numeroKm));
+
 
 // 2.chiedere l'età del passeggero.//
-var eta = prompt("quanti anni hai ?")
+var eta = parseInt(prompt("quanti anni hai ?"));
+console.log(typeof(eta));
+
 
 // 3.chiedere il prezzo totale del viaggio.//
 var prezzoBase = 0.21 * numeroKm;
-alert("prezzo base senza sconto" + prezzoBase);
+document.getElementById("prezzo Base").innerHTML += prezzoBase;
+
 
 // 4.sconto minorenni//
-var scontoMinorenni = prezzoBase / 100 * 20;
-alert("sconto minorenni" + scontoMinorenni );
+var scontoMinorenni = prezzoBase / 100 * 30;
+// 5.sconto over 65//
+var scontoOver65 = prezzoBase / 100 * 50;
 
-// 4.sconto over 65//
-var scontoOver65 = prezzoBase / 100 * 40;
-alert("over 65 " + scontoOver65);
 
-// 5.prezzo prezzoMinorenni//
+// 6.prezzo prezzoMinorenni//
 var prezzoMinorenni = prezzoBase - scontoMinorenni;
+document.getElementById("prezzo Minorenni").innerHTML += prezzoMinorenni;
 
-// 6.prezzoOver65//
+// .prezzoOver65//
 var prezzoOver65 = prezzoBase - scontoOver65;
+document.getElementById("prezzo Over65").innerHTML += prezzoOver65;  
+
+
+
+
+
 
 
 if (eta < 18) {
-    alert("sconti minorenni dato " + prezzoMinorenni);
+    // alert("sconti minorenni dato " + prezzoMinorenni);
 
 } else if (eta > 65) {
-    alert("sconti over 65 dato " + prezzoOver65);
+    // alert("sconti over 65 dato " + prezzoOver65);
 
 } else {
-    alert("prezzo base " + prezzoBase);
+    // alert("prezzo base " + prezzoBase);
 }
 
 
 
-
-// il prezzo del biglietto è definito in base ai km(0.21 € al km)//
-
-
-
-
-
-// va applicato uno sconto del 20 % per i minorenni
-// va applicato uno sconto del 40 % per gli over 65.
