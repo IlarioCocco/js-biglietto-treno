6414666e61ca922cf280194e9b93e6207571fc0a
IlarioCOCCO <ilario.cocco@gmail.com>
2022-02-07 12:24:59 +0100

    recap basic

--- js/scripts.js
+++ js/scripts.js
@@ -2,51 +2,39 @@
 // va applicato uno sconto del 30 % per i minorenni
 // va applicato uno sconto del 50 % per gli over 65.
 
-
 // 1.chiedere all'utente il numero di chilometri che vuole percorrere.//
 var numeroKm = parseInt(prompt("quanti km vuoi percorrere ?"));
 console.log(typeof(numeroKm));
-
 
 // 2.chiedere l'età del passeggero.//
 var eta = parseInt(prompt("quanti anni hai ?"));
 console.log(typeof(eta));
 
-
-// 3.chiedere il prezzo totale del viaggio.//
+// 3.prezzo totale a km effettivo.//
 var prezzoBase = 0.21 * numeroKm;
-document.getElementById("prezzo Base").innerHTML += prezzoBase;
-
+document.getElementById("prezzo Base").innerHTML += prezzoBase + " € " + "a Km";
 
 // 4.sconto minorenni//
 var scontoMinorenni = prezzoBase / 100 * 30;
 // 5.sconto over 65//
 var scontoOver65 = prezzoBase / 100 * 50;
 
-
 // 6.prezzo prezzoMinorenni//
 var prezzoMinorenni = prezzoBase - scontoMinorenni;
-document.getElementById("prezzo Minorenni").innerHTML += prezzoMinorenni;
+document.getElementById("prezzo Minorenni").innerHTML += prezzoMinorenni + " € ";
 
 // .prezzoOver65//
 var prezzoOver65 = prezzoBase - scontoOver65;
-document.getElementById("prezzo Over65").innerHTML += prezzoOver65;  
-
-
-
-
-
-
+document.getElementById("prezzo Over65").innerHTML += prezzoOver65 + " € ";  
 
 if (eta < 18) {
-    // alert("sconti minorenni dato " + prezzoMinorenni);
+    alert("prezzo per minorenni: " + prezzoMinorenni + " € ");
 
 } else if (eta > 65) {
-    // alert("sconti over 65 dato " + prezzoOver65);
+    alert("prezzo per over 65 è di: " + prezzoOver65 + " € ");
 
 } else {
-    // alert("prezzo base " + prezzoBase);
+    alert("prezzo base: " + prezzoBase + " € ");
 }
-
 
 
